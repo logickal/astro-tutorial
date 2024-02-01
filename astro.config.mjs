01a6e214f3dae50470734c3ff88a667647ba5df6
Jeremy Dickens <jeremy.dickens@pantheon.io>
2024-02-01 15:48:15 +0000

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://exampleastro.com",
+  site: "https://exampleastro.com",
+  integrations: [preact()]
 });
